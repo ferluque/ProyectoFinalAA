76ba76a08757021324c4f5e8e140035b5318338b
ferluque <fl1562001@go.ugr.es>
2022-06-17 19:08:38 +0200

    Update

--- proyecto.py
+++ proyecto.py
@@ -116,7 +116,7 @@ for i in range(size):
     
 #Media de valores fuera del rango acotado
 media_outliers  = np.mean(resultado,axis=0)
-print(media_outliers)
+print("Hay un "+ str(media_outliers) + "% de outliers.")
 
 
 '''
@@ -183,6 +183,52 @@ plt.show()
 ###############################################################################
 
 
+###############################################################################
+###############################################################################
+# ELIMINACIÓN DE OUTLIERS 
+###############################################################################
+###############################################################################
+
+#Voy a intentar eliminar los outliers mediante las 2 técticas menos invasivas
+# que hemos podido encontrar
+#   - Media imputation
+#   - Acotación mediante cuartiles
+
+# size = len(desc.columns)
+
+# for i in range(size):
+#     tenth_percentile = np.percentile(dfX[i], 10)
+#     ninetieth_percentile = np.percentile(dfX[i], 90)
+    
+#     mean = dfX[i].mean()
+    
+    
+#     Q1 = desc[i][4]
+#     Q3 = desc[i][6]
+#     IQR = Q3-Q1
+#     factor = IQR*1.5 
+#     upper_bound = Q3 + factor
+#     lower_bound = Q1 - factor
+    
+#     ###############################################################################
+#     #           QUARTILE IMPUTATION
+#     ###############################################################################
+#     # dfX[i] = np.where(dfX[i]  > upper_bound, ninetieth_percentile, dfX[i])
+#     # dfX[i]  = np.where(dfX[i] < lower_bound, tenth_percentile, dfX[i])
+#     # ###############################################################################
+#     # #           MEDIAN IMPUTATION
+#     # ###############################################################################
+#     # dfX[i] = np.where(dfX[i]  > upper_bound, mean, dfX[i])
+#     # dfX[i]  = np.where(dfX[i] < lower_bound, mean, dfX[i])
+
+# # X = dfX.to_numpy()
+
+###############################################################################
+###############################################################################
+# FIN ELIMINACIÓN DE OUTLIERS
+###############################################################################
+###############################################################################
+
 
 ###############################################################################
 ###############################################################################
@@ -208,6 +254,95 @@ plt.show()
 ###############################################################################
 ###############################################################################
 
+###############################################################################
+###############################################################################
+# ANÁLISIS REGRESIÓN LOGÍSTICA
+###############################################################################
+###############################################################################
+
+## Análisis max_iter
+y_clases = np.array(y_cod).reshape((1,-1))[0]
+
+# iters = []
+# aciertos_maxiter = []
+# for i in range(300,1000,100):
+#     iters.append(i)
+#     aciertos_maxiter.append(cross_val_score(SGDClassifier(loss='log',max_iter=i,n_jobs=-1),X,y_clases,n_jobs=-1,scoring='accuracy').mean())
+
+# plt.ylabel("Accuracy de validación medio")
+# plt.xlabel("Número de iteraciones")
+# plt.title("Accuracy de Regresión Logística en función del número máximo de iteraciones")
+# plt.plot(iters,aciertos_maxiter)
+
+## Regularización y valores de lambda
+# regularization = {'penalty':['l1','l2'],
+#                   'alpha':[1e-6,1e-5,1e-4,1e-3,1e-2,1e-1]}
+
+# regularization_GridSearch = GridSearchCV(SGDClassifier(loss='log',max_iter=300, n_jobs=-1),regularization,scoring='accuracy',n_jobs=-1)
+# regularization_GridSearch.fit(X,y_clases)
+# print("Mejores valores para regularización: ", regularization_GridSearch.best_params_)
+# print("Con un accuracy de: ", regularization_GridSearch.best_score_)
+
+
+## Learning rate
+# aciertos_lrs = {'constant':0.0,'optimal':0.0,'invscaling':0.0,'adaptive':0.0}
+
+# aciertos_lrs['constant'] = cross_val_score(SGDClassifier(loss='log',max_iter=300,penalty='l1',alpha=0.001,n_jobs=-1,learning_rate='constant',eta0=0.1),X,y_clases,n_jobs=-1,scoring='accuracy').mean()
+# aciertos_lrs['optimal'] = cross_val_score(SGDClassifier(loss='log',max_iter=300,penalty='l1',alpha=0.001,n_jobs=-1,learning_rate='optimal',eta0=0.1),X,y_clases,n_jobs=-1,scoring='accuracy').mean()
+# aciertos_lrs['invscaling'] = cross_val_score(SGDClassifier(loss='log',max_iter=300,penalty='l1',alpha=0.001,n_jobs=-1,learning_rate='invscaling',eta0=0.1),X,y_clases,n_jobs=-1,scoring='accuracy').mean()
+# aciertos_lrs['adaptive'] = cross_val_score(SGDClassifier(loss='log',max_iter=300,penalty='l1',alpha=0.001,n_jobs=-1,learning_rate='adaptive',eta0=0.1),X,y_clases,n_jobs=-1,scoring='accuracy').mean()
+
+
+# plt.ylabel("Accuracy de validación medio")
+# plt.xlabel("Tipo de learning rate")
+# plt.title("Accuracy de Regresión Logística en función del tipo de learning rate")
+# plt.bar(list(aciertos_lrs.keys()),aciertos_lrs.values())
+# plt.show()
+
+# # Learning rate inicial
+# aciertos_eta0 = []
+# etas0=[]
+# eta0=1e-3
+# for i in range(4):
+#     aciertos_eta0.append(cross_val_score(SGDClassifier(loss='log',max_iter=300,penalty='l1',alpha=0.001,n_jobs=-1,learning_rate='adaptive',eta0=eta0),X,y_clases,n_jobs=-1,scoring='accuracy').mean())
+#     etas0.append(eta0)
+#     eta0*=10
+
+# plt.ylabel("Accuracy de validación medio")
+# plt.xlabel("Valor de eta0")
+# plt.title("Accuracy de Regresión Logística en función del valor de learning rate inicial")
+# plt.xscale('log')
+# plt.plot(etas0,aciertos_eta0)
+# plt.show()
+
+## Early stopping
+# aciertos_es = {True: 0.0, False: 0.0}
+# indices = [True,False]
+# for i in range(2):
+#     aciertos_es[indices[i]] = cross_val_score(SGDClassifier(loss='log',max_iter=300,penalty='l1',alpha=0.001,n_jobs=-1,learning_rate='adaptive',eta0=0.01, early_stopping=indices[i]),X,y_clases,n_jobs=-1,scoring='accuracy').mean()
+
+# plt.ylabel("Accuracy de validación medio")
+# plt.xlabel("Early stopping")
+# plt.title("Accuracy de Regresión Logística en función del valor de early stopping")
+# plt.bar(list(aciertos_es.keys()),aciertos_es.values())
+# plt.show()
+
+## n_iter_no_change y tol
+tol = {'tol':[1e-5,1e-4,1e-3,1e-2,1e-1],
+                  'n_iter_no_change':[3,4,5,6,7]}
+
+tol_GridSearch = GridSearchCV(SGDClassifier(loss='log',max_iter=300,penalty='l1',alpha=0.001,n_jobs=-1,learning_rate='adaptive',eta0=0.01),tol,scoring='accuracy',n_jobs=-1)
+tol_GridSearch.fit(X,y_clases)
+print("Mejores valores para tol y n_iter_no_change: ", tol_GridSearch.best_params_)
+print("Con un accuracy de: ", tol_GridSearch.best_score_)
+
+
+###############################################################################
+###############################################################################
+# FIN ANÁLISIS REGRESIÓN LOGÍSTICA
+###############################################################################
+###############################################################################
+
 
 ###############################################################################
 ###############################################################################
@@ -215,19 +350,43 @@ plt.show()
 ###############################################################################
 ###############################################################################
 
-y_clases = np.array(y_cod).reshape((1,-1))[0]
+# y_clases = np.array(y_cod).reshape((1,-1))[0]
+# errores = 7*[0]
+# # print("Todo con hiperparámetros por defecto")
+# errores[0] = cross_val_score(SGDClassifier(loss='log',n_jobs=-1),X,y_clases,n_jobs=-1,scoring='accuracy')
+# # print("Ecv de SGDClassifier con Log error: ",errores[0] )
+# errores[1] =cross_val_score(SGDClassifier(loss='perceptron',n_jobs=-1),X,y_clases,n_jobs=-1,scoring='accuracy')
+# # print("Ecv de SGDClassifier perceptron: ", errores[1] )
+# errores[2] =cross_val_score(MLPClassifier(learning_rate='adaptive', warm_start=True,max_iter=1000),X,y_clases,n_jobs=-1,scoring='accuracy')
+# # print("Ecv de MLP: ", errores[2] )
+# errores[3] =cross_val_score(SVC(kernel='poly'),X,y_clases,n_jobs=-1,scoring='accuracy')
+# # print("Ecv de SVC kernel polinomial: ",errores[3]  )
+# errores[4] =cross_val_score(SVC(kernel='rbf'),X,y_clases,n_jobs=-1,scoring='accuracy')
+# # print("Ecv de SVC kernel RBF-gaussiano: ", errores[4] )
+# errores[5] = cross_val_score(GradientBoostingClassifier(n_estimators=10), X,y_clases,scoring='accuracy', n_jobs=-1)
+# # print("Ecv de Boosting: ",errores[5] )
+# errores[6] =cross_val_score(RandomForestClassifier(n_estimators=500), X,y_clases,scoring='accuracy', n_jobs=-1)
+# # print("Ecv de Random Forest: ",errores[6]  )
+# # Este último es el que mejor accuracy ha dado con hasta un 50%
+
+# media = len(errores)*[0]
+# for i in range(len(errores)):
+#     media[i] = errores[i].mean()
+    
+
 # print("Todo con hiperparámetros por defecto")
-# print("Ecv de SGDClassifier con Log error: ", cross_val_score(SGDClassifier(loss='log',n_jobs=-1),X,y_clases,n_jobs=-1,scoring='accuracy'))
-# print("Ecv de SGDClassifier perceptron: ", cross_val_score(SGDClassifier(loss='perceptron',n_jobs=-1),X,y_clases,n_jobs=-1,scoring='accuracy'))
-# print("Ecv de MLP: ", cross_val_score(MLPClassifier(learning_rate='adaptive', warm_start=True,max_iter=500),X,y_clases,n_jobs=-1,scoring='accuracy'))
-# print("Ecv de SVC kernel polinomial: ", cross_val_score(SVC(kernel='poly'),X,y_clases,n_jobs=-1,scoring='accuracy'))
-# print("Ecv de SVC kernel RBF-gaussiano: ", cross_val_score(SVC(kernel='rbf'),X,y_clases,n_jobs=-1,scoring='accuracy'))
-# print("Ecv de Boosting: ", cross_val_score(GradientBoostingClassifier(n_estimators=10), X,y_clases,scoring='accuracy', n_jobs=-1))
-# print("Ecv de Random Forest: ", cross_val_score(RandomForestClassifier(n_estimators=500), X,y_clases,scoring='accuracy', n_jobs=-1))
-# Este último es el que mejor accuracy ha dado con hasta un 50%
+# print("Ecv de SGDClassifier con Log error: ",media[0] )
+# print("Ecv de SGDClassifier perceptron: ", media[1] )
+# print("Ecv de MLP: ", media[2] )
+# print("Ecv de SVC kernel polinomial: ",media[3]  )
+# print("Ecv de SVC kernel RBF-gaussiano: ", media[4] )
+# print("Ecv de Boosting: ",media[5] )
+# print("Ecv de Random Forest: ",media[6])
 
+# modelos = ['SGDClassifier+log','SGDClassifier perceptron','MLP','SVC kernel polinomial','SVC kernel RBF-gaussiano','Boosting','Random Forest']
 
-
+# df1 = pd.DataFrame(dict([("Modelos",modelos),("Media error CV",media)]))
+# print(df1.style.to_latex()) #FORMATO LATEX
 
 ###############################################################################
 ###############################################################################
@@ -266,7 +425,7 @@ y_clases = np.array(y_cod).reshape((1,-1))[0]
 # print(MLP_GridSearch.cv_results_)
 
 # Los mejores parámetros de los estudiados han sido
-# {'activation': 'relu', 'alpha': 0.001, 'hidden_layer_sizes': [100], 'solver': 'adam'}
+#{'activation': 'relu', 'alpha': 0.001, 'hidden_layer_sizes': [100], 'solver': 'adam'}
 # print(cross_val_score(MLPClassifier(max_iter=500,alpha=1e-3,hidden_layer_sizes=(100,100)),X,y_clases,scoring='accuracy'))
 
 ###############################################################################
@@ -287,5 +446,5 @@ y_clases = np.array(y_cod).reshape((1,-1))[0]
 
 # Los mejores parámetros son
 # {'criterion': 'gini', 'n_estimators': 500, 'warm_start': True}
-print(cross_val_score(RandomForestClassifier(criterion='gini',n_estimators=500,warm_start=True),X,y_clases,scoring='accuracy',n_jobs=-1))
+# print(cross_val_score(RandomForestClassifier(criterion='gini',n_estimators=500,warm_start=True),X,y_clases,scoring='accuracy',n_jobs=-1))
 
